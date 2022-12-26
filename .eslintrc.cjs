f7bf9f42c1e4c254db9d9ab7ec841c8ac2c65b9c
Sea Kimhour <seakimhour@gmail.com>
2022-12-26 14:10:50 +0900

    add tailwindcss and self compile to dist

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,6 +7,7 @@ module.exports = {
     "plugin:vue/vue3-essential",
     "eslint:recommended",
     "@vue/eslint-config-prettier",
+    "plugin:tailwindcss/recommended",
   ],
   parserOptions: {
     ecmaVersion: "latest",
