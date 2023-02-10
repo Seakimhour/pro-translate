18119b6c77967a6c47c08c446a69a0283c4d03d2
seakimhour <seakimhour@gmail.com>
2023-02-10 15:29:51 +0900

    change fetch to axios

--- src/js/translate.js
+++ src/js/translate.js
@@ -1,7 +1,9 @@
+import axios from "axios";
+
 export const translateAPI = async (text, sourceLanguage, targetLanguage) => {
   const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${sourceLanguage}&tl=${targetLanguage}&dt=t&q=${encodeURIComponent(
     text
   )}`;
-  const response = await fetch(url).catch((error) => error.response);
-  return response.json();
+  const response = await axios.get(url).catch((error) => error.response);
+  return response.data;
 };
