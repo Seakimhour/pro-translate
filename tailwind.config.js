53073e20eff9910e69c4c5d34a30fe8d0922d4b7
Sea Kimhour <seakimhour@gmail.com>
2023-02-14 20:10:07 +0900

    convert rem to px

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,31 @@
 /* eslint-env node */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+function rem2px(input, fontSize = 16) {
+  if (input == null) {
+    return input;
+  }
+  switch (typeof input) {
+    case "object":
+      if (Array.isArray(input)) {
+        return input.map((val) => rem2px(val, fontSize));
+      } else {
+        const ret = {};
+        for (const key in input) {
+          ret[key] = rem2px(input[key]);
+        }
+        return ret;
+      }
+    case "string":
+      return input.replace(
+        /(\d*\.?\d+)rem$/,
+        (_, val) => parseFloat(val) * fontSize + "px"
+      );
+    default:
+      return input;
+  }
+}
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   corePlugins: {
@@ -7,6 +34,19 @@ module.exports = {
   content: ["./src/**/*.{html,js,vue}"],
   important: "#pro-translate",
   theme: {
+    borderRadius: rem2px(defaultTheme.borderRadius),
+    columns: rem2px(defaultTheme.columns),
+    fontSize: rem2px(defaultTheme.fontSize),
+    lineHeight: rem2px(defaultTheme.lineHeight),
+    spacing: rem2px(defaultTheme.spacing),
+    maxWidth: ({ theme, breakpoints }) => ({
+      ...rem2px(defaultTheme.maxWidth({ theme, breakpoints })),
+    }),
+    minHeight: {
+      ...rem2px(defaultTheme.minHeight),
+      4: "16px",
+      8: "32px",
+    },
     extend: {
       colors: {
         primary: "#539E8A",
